d1bf5c4444861b41da9361ba77d7cfdfc5e10f11
elisabeth-yolanda <97127091+elisabeth-yolanda@users.noreply.github.com>
2023-07-24 14:05:37 +0700

    Update Motto.vue

--- resources/js/components/Motto.vue
+++ resources/js/components/Motto.vue
@@ -8,7 +8,9 @@
         border-radius: 100rem;
         width: 20%;
         border: 1rem solid #363635;
-        position: relative;"
+        position: relative;
+        transition: 2s all ease;
+      "
         :style="styleParent"
     >
       <div
@@ -31,13 +33,13 @@
           justify-content: center;
           top: -40px;
           right: 30px;
-          transition: 0.7s all ease;
+          transition: 5s all ease;
         "
         :style="styleVision"
       >
         Vision
       </div>
-
+      
       <div
         role="button"
         @click="
@@ -58,12 +60,13 @@
           justify-content: center;
           bottom: 75px;
           left: -55px;
-           transition: 0.7s all ease;
+          transition: 5s all ease;
         "
         :style="styleMission"
+        
       >
         Mission
-      </div>
+    </div>      
 
       <div
         role="button"
@@ -85,7 +88,7 @@
           justify-content: center;
           bottom: -31px;
           right: 8px;
-          transition: 0.7s all ease;
+          transition: 5s all ease;
         "
         :style="styleValue"
       >
@@ -134,54 +137,62 @@ export default {
         switch(this.motto) {
             case 'Vision' :
             if(this.widthSize < 770) {
-                this.classname = "row justify-content-md-end justify-content-center align-items-center gap-5";
+                this.classname = "row justify-content-md-end justify-content-center align-items-center gap-5 translateX-100% v-slide-x-transition=20";
                 this.styleVision = "transform: rotate(202deg)";
                 this.styleMission = "transform: rotate(200deg)";
                 this.styleValue = "transform: rotate(200deg)";
-                this.styleParent = "transform: rotate(160deg); transition: 1s all ease;";
+                this.styleParent = "transform: rotate(160deg)";
             }else {
-                this.classname = "row justify-content-md-end justify-content-center align-items-center gap-5";
+                this.classname = "row justify-content-md-end justify-content-center align-items-center gap-5 translateX-100% v-slide-x-transition=20";
                 this.styleVision = "transform: rotate(290deg)";
                 this.styleMission = "transform: rotate(291deg)";
                 this.styleValue = "transform: rotate(294deg)";
-                this.styleParent = "transform: rotate(70deg); transition: 1s all ease;";
+                this.styleParent = "transform: rotate(70deg)";
             }
             break;
             case "Mission":
             if(this.widthSize < 770) {
-                this.classname = "row justify-content-md-end justify-content-center align-items-center gap-5";
+                this.classname = "row justify-content-md-end justify-content-center align-items-center gap-5 translateX-100% v-slide-x-transition=20";
                 this.styleVision = "transform: rotate(90deg)";
                 this.styleMission = "transform: rotate(90deg)";
                 this.styleValue = "transform: rotate(90deg)";
-                this.styleParent = "transform: rotate(269deg); transition: 0.8s all fade-out;";
+                this.styleParent = "transform: rotate(269deg)";
             }else {
-                this.classname = "row justify-content-md-end justify-content-center align-items-center gap-5";
+                this.classname = "row justify-content-md-end justify-content-center align-items-center gap-5 translateX-100% v-slide-x-transition=20";
+
                 this.styleVision = "transform: rotate(192deg)";
                 this.styleMission = "transform: rotate(189deg)";
                 this.styleValue = "transform: rotate(189deg)";
-                this.styleParent = "transform: rotate(170deg); transition: 0.8s all fade-out;";
+                this.styleParent = "transform: rotate(170deg)";
             }
             break
             case "Value" :
             if(this.widthSize < 770) {
-                this.classname = "row justify-content-md-end justify-content-center align-items-center gap-5";
+                this.classname = "row justify-content-md-end justify-content-center align-items-center gap-5 translateX-100% v-slide-x-transition=20";
                 this.styleVision = "transform: rotate(329deg)";
                 this.styleMission = "transform: rotate(326deg)";
                 this.styleValue = "transform: rotate(330deg)";
-                this.styleParent = "transform: rotate(32deg); transition: 1.5s all fade-out;";
+                this.styleParent = "transform: rotate(32deg)";
             }else {
-                this.classname = "row justify-content-md-end justify-content-center align-items-center gap-5";
+                this.classname = "row justify-content-md-end justify-content-center align-items-center gap-5 translateX-100% v-slide-x-transition=20";
                 this.styleVision = "transform: rotate(57deg)";
                 this.styleMission = "transform: rotate(58deg)";
                 this.styleValue = "transform: rotate(54deg)";
-                this.styleParent = "transform: rotate(304deg); transition: 1.5s all fade-out;";
+                this.styleParent = "transform: rotate(304deg)";
             }
-            break
+            break      
+         
         }
+
     },
+    
+    
     updateWindowWidth() {
       this.widthSize = window.innerWidth;
     }
   },
+
 };
+
+
 </script>
